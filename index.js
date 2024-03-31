6296275f0d321d4bf589c2d43572ade42590b136
Gideon <kariukikibugi@gmail.com>
2024-03-31 21:47:01 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const divide = (num1 = 2000, num2 = 100) => num1 / num2;
-const square = num3 => num3 * num3;
-const add = (num4, num5) => num4 + num5;
+const square = num2 => num2 * num2;
+const add = (num3, num4) => num3 + num4;
